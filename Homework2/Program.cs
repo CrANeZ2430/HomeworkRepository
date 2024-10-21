a0f57bbf63b3474a520712e2649cb504a6abbf30
CrnZ1675 <crn.alexei@gmail.com>
2024-10-22 00:21:18 +0300

    Added 2 homework

---
+++ Homework2/Program.cs
@@ -0,0 +1,46 @@
+﻿public class DayOfWeekNaming
+{
+    private static byte weekDayNum;
+
+    private static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter the day of week (1-7): ");
+
+            if (!byte.TryParse(Console.ReadLine(), out weekDayNum) || 1 > weekDayNum || weekDayNum > 7)
+            {
+                Console.WriteLine("Incorect day of week input");
+                continue;
+            }
+
+            string weekDayText = GetDayOfWeekByNum(weekDayNum);
+            Console.WriteLine(weekDayText);
+        }
+    }
+
+    private static string GetDayOfWeekByNum(byte number)
+    {
+        switch (number)
+        {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return "Incorrect day of week";
+        }
+    }
+}
+
+
