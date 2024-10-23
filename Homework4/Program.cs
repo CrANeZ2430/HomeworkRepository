1fbb7003ac5841a9652cf94bc8854de1ea5a14ad
CrnZ1675 <crn.alexei@gmail.com>
2024-10-23 21:04:21 +0300

    Added fourth and fifth homeworks

---
+++ Homework4/Program.cs
@@ -0,0 +1,41 @@
+﻿public class CheckForNullException
+{
+    private static decimal firstNum;
+    private static decimal secondNum;
+    private static decimal resultNum;
+
+    private static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Dividing Calculator");
+
+            Console.Write("\nEnter your first number: ");
+            while (!decimal.TryParse(Console.ReadLine(), out firstNum))
+            {
+                Console.WriteLine("Enter valid number");
+                Console.Write("Enter your first number: ");
+                continue;
+            }
+            Console.Write("Enter your second number: ");
+            while (!decimal.TryParse(Console.ReadLine(), out secondNum))
+            {
+                Console.WriteLine("Enter valid number");
+                Console.Write("Enter your second number: ");
+                continue;
+            }
+
+            Console.Write("\nThe result is: ");
+            try
+            {
+                resultNum = firstNum / secondNum;
+                Console.WriteLine(resultNum);
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("You cannot divide by zero. Reenter your digits\n");
+            }
+        }
+    }
+}
---
+++ Homework5/Program.cs
@@ -0,0 +1,24 @@
+﻿public class CheckForInvalidNumberException
+{
+    private static byte userAge;
+
+    private static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter user age: ");
+
+            try
+            {
+                userAge = byte.Parse(Console.ReadLine()!);
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("Invalid age numder. Reenter user age\n");
+            }
+        }
+
+        Console.WriteLine($"User's age is {userAge}");
+    }
+}
