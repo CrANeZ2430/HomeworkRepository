5a47dd3683db0d1e06fa16a9a5cc895441fc86ca
CrnZ1675 <crn.alexei@gmail.com>
2024-11-06 18:43:18 +0200

    Reworked conditions for win in 7 homework

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -24,12 +24,14 @@ void SetPlayerChoiceOnField(string playerChoice, byte playerNumber)
 {
     while (true)
     {
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.Write($"\n{playerNumber}. player choice: ");
         string playerChoiceOnField = Console.ReadLine()!;
         Console.WriteLine();
         if (int.TryParse(playerChoiceOnField, out int number) && number > 0 && number < 10 && field[int.Parse(playerChoiceOnField) - 1] == playerChoiceOnField)
         {
             field[number - 1] = playerChoice;
+            Console.ResetColor();
             BuildField();
             break;
         }
@@ -57,61 +59,41 @@ bool CheckForWinnerAndDraw()
 {
     for (int i = 0; i < 3; i++)
     {
-        if (field[3 * i] == "X" && field[3 * i + 1] == "X" && field[3 * i + 2] == "X")
+        bool firstPlayerWin = (field[3 * i] == "X" && field[3 * i + 1] == "X" && field[3 * i + 2] == "X") ||
+                              (field[i] == "X" && field[i + 3] == "X" && field[i + 6] == "X") ||
+                              (field[0] == "X" && field[4] == "X" && field[8] == "X") ||
+                              (field[2] == "X" && field[4] == "X" && field[6] == "X");
+        bool secondPlayerWin = (field[3 * i] == "O" && field[3 * i + 1] == "O" && field[3 * i + 2] == "O") ||
+                               (field[i] == "O" && field[i + 3] == "O" && field[i + 6] == "O") ||
+                               (field[0] == "O" && field[4] == "O" && field[8] == "O") ||
+                               (field[2] == "O" && field[4] == "O" && field[6] == "O");
+        if (firstPlayerWin)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nFirst player won!");
             return true;
         }
-        if (field[3 * i] == "O" && field[3 * i + 1] == "O" && field[3 * i + 2] == "O")
+        if (secondPlayerWin)
         {
-            Console.WriteLine("\nSecond player won!");
-            return true;
-        }
-        if (field[i] == "X" && field[i + 3] == "X" && field[i + 6] == "X")
-        {
-            Console.WriteLine("\nFirst player won!");
-            return true;
-        }
-        if (field[i] == "O" && field[i + 3] == "O" && field[i + 6] == "O")
-        {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nSecond player won!");
             return true;
         }
     }
 
-    if (field[0] == "X" && field[4] == "X" && field[8] == "X")
-    {
-        Console.WriteLine("\nFirst player won!");
-        return true;
-    }
-    if (field[0] == "O" && field[4] == "O" && field[8] == "O")
-    {
-        Console.WriteLine("\nSecond player won!");
-        return true;
-    }
-    if (field[2] == "X" && field[4] == "X" && field[6] == "X")
-    {
-        Console.WriteLine("\nFirst player won!");
-        return true;
-    }
-    if (field[2] == "O" && field[4] == "O" && field[6] == "O")
-    {
-        Console.WriteLine("\nSecond player won!");
-        return true;
-    }
-
-    int counter = 0;
+    int fieldUnitsFilled = 0;
 
     foreach (string slot in field)
     {
         if (slot == "X" || slot == "O")
         {
-            counter++;
+            fieldUnitsFilled++;
         }
     }
 
-    if (counter == 9)
+    if (fieldUnitsFilled == 9)
     {
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("\nDraw!");
         return true;
     }
