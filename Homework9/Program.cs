4dccc5840e6166c569505334213512891ed0a09d
CrnZ1675 <crn.alexei@gmail.com>
2024-11-08 23:31:30 +0200

    Added 9 homework template

---
+++ Homework9/Program.cs
@@ -0,0 +1,48 @@
+﻿byte homeworkNumber;
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Enter the number of homework: ");
+    if (byte.TryParse(Console.ReadLine(), out homeworkNumber) && homeworkNumber <= 4)
+    {
+        Console.ResetColor();
+        break;
+    }
+}
+
+switch (homeworkNumber)
+{
+    case 1:
+        FirstHomework();
+        break;
+    case 2:
+        SecondHomework();
+        break;
+    case 3:
+        ThirdHomework();
+        break;
+    case 4:
+        FourthHomework();
+        break;
+}
+
+void FirstHomework()
+{
+    
+}
+
+void SecondHomework()
+{
+    
+}
+
+void ThirdHomework()
+{
+    
+}
+
+void FourthHomework()
+{
+    
+}
