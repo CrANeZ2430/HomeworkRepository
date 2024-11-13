e88843183fd81b940a0be65bd89d89c8b1833b1e
CrnZ1675 <crn.alexei@gmail.com>
2024-11-13 16:18:40 +0200

    Added 10 homework

---
+++ Homework10/Program.cs
@@ -0,0 +1,27 @@
+﻿string initials;
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write("Enter your name: ");
+    initials = Console.ReadLine();
+
+    if (initials.Length != 0 && initials.Contains(' '))
+    {
+        Console.ResetColor();
+        break;
+    }
+    Console.Clear();
+}
+
+string[] trimmedInput = initials.Split().Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+Console.WriteLine($"Your initials: {trimmedInput[0]} {trimmedInput[1]}");
+
+if (trimmedInput[0][0] == trimmedInput[1][0])
+{
+    Console.WriteLine("Your initials start with the same letter");
+}
+else
+{
+    Console.WriteLine("Your initials do not start with the same letter");
+}
