dda1e37617b0c65ee47f2bd6d574eb976204cf72
CrnZ1675 <crn.alexei@gmail.com>
2024-11-22 22:50:35 +0200

    Added unit tests

--- Homework12/Program.cs
+++ Homework12/Program.cs
@@ -1,43 +1,45 @@
-﻿int fibonacciNumberOrder;
-
-while (true)
+﻿while (true)
 {
-    Console.Write("Enter Fibonacci number: ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write("Enter digit of the Fibonacci number: ");
     string input = Console.ReadLine()!;
-    if (int.TryParse(input, out fibonacciNumberOrder) && fibonacciNumberOrder > 0)
+    if (int.TryParse(input, out int fibonacciNumberOrder))
     {
-        AdditionalMath.CalculateFibonacciNumber(fibonacciNumberOrder);
+        Console.ResetColor();
+        Console.WriteLine("Your Fibonacci number: " + AdditionalMath.CalculateFibonacciNumber(fibonacciNumberOrder));
         break;
     }
 }
 
-static class AdditionalMath
+public static class AdditionalMath
 {
-    private static ulong temp1 = 0;
-    private static ulong temp2 = 1;
+    private static long currentNumber;
+    private static long nextNumber = 1;
+    private const int ExceptedVars = 2;
     
-    public static void CalculateFibonacciNumber(int number)
+    public static long CalculateFibonacciNumber(int number)
     {
+        if (number < 0)
+        {
+            Console.WriteLine("The number cannot be negative");
+            return -1;
+        }
         if (number == 1)
         {
-            Console.WriteLine("Fibonacci number: 0");
-            return;
+            return 0;
         }
         if (number == 2)
         {
-            Console.WriteLine("Fibonacci number: 1");
-        }
-        
-        if (number - 3 == 0)
-        {
-            Console.WriteLine($"Fibonacci number: {temp1 + temp2}");
-            return;
+            return 1;
         }
 
-        ulong temp01 = temp1;
-        ulong temp02 = temp2;
-        temp1 = temp02;
-        temp2 = temp01 + temp02;
-        CalculateFibonacciNumber(number - 1);
+        long previousNumber = currentNumber;
+        currentNumber = nextNumber;
+        nextNumber = previousNumber + nextNumber;
+        if (number - ExceptedVars != 0)
+        {
+            CalculateFibonacciNumber(number - 1);
+        }
+        return nextNumber;
     }
 }
---
+++ Homework12Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿namespace Homework12Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CalculateFibonacciNumber_CheckCommonValue()
+    {
+        // Assert
+        const int orderNumber = 11;
+        const long expectedResult = 55;
+        
+        //Arrange
+
+        long fibonacciNumber = AdditionalMath.CalculateFibonacciNumber(orderNumber);
+
+        //Act
+
+        Assert.True(fibonacciNumber == expectedResult);
+    }
+    
+    [Fact]
+    public void CalculateFibonacciNumber_CheckNegativeValue()
+    {
+        // Assert
+        const int orderNumber = -10;
+        const long expectedResult = -1;
+        
+        //Arrange
+        long fibonacciNumber = AdditionalMath.CalculateFibonacciNumber(orderNumber);
+
+        //Act
+        Assert.True(fibonacciNumber == expectedResult);
+    }
+    
+    [Fact]
+    public void CalculateFibonacciNumber_CheckSpecialValues()
+    {
+        // Assert
+        const int orderNumber1 = 1;
+        const long expectedResult1 = 0;
+        
+        const int orderNumber2 = 2;
+        const long expectedResult2 = 1;
+        
+        //Arrange
+        long fibonacciNumber = AdditionalMath.CalculateFibonacciNumber(orderNumber1);
+
+        //Act
+        Assert.True(fibonacciNumber == expectedResult1);
+        
+        //Arrange
+        fibonacciNumber = AdditionalMath.CalculateFibonacciNumber(orderNumber2);
+
+        //Act
+        Assert.True(fibonacciNumber == expectedResult2);
+    }
+}
