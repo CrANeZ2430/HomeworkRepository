bc565153f6f3476b36d9e597256cd035ca3fb7e0
CrnZ1675 <crn.alexei@gmail.com>
2024-11-22 22:02:26 +0200

    Revert "First version of homework12

---
+++ Homework12/Homework12/Program.cs
@@ -0,0 +1,33 @@
+﻿AdditionalMath.CalculateFibonacciNumber(110);
+
+static class AdditionalMath
+{
+    private static ulong temp1 = 0;
+    private static ulong temp2 = 1;
+    
+    public static void CalculateFibonacciNumber(int number)
+    {
+        if (number == 1)
+        {
+            Console.WriteLine("Fibonacci number: 0");
+            return;
+        }
+
+        if (number == 2)
+        {
+            Console.WriteLine("Fibonacci number: 1");
+        }
+        
+        if (number - 3 == 0)
+        {
+            Console.WriteLine($"Fibonacci number: {temp1 + temp2}");
+            return;
+        }
+
+        ulong temp01 = temp1;
+        ulong temp02 = temp2;
+        temp1 = temp02;
+        temp2 = temp01 + temp02;
+        CalculateFibonacciNumber(number - 1);
+    }
+}
