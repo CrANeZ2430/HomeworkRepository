0ad2d1669f33a0527e15e0887480a484db0773a2
CrnZ1675 <crn.alexei@gmail.com>
2024-12-04 00:13:35 +0200

    Made homework 16: copy text from one file to another

---
+++ Homework16/Program.cs
@@ -0,0 +1,56 @@
+﻿string sourcePath;
+string destinationPath;
+
+while (true)
+{
+    while (true)
+    {
+        Console.Write("Enter source file path: ");
+        sourcePath = Console.ReadLine()!;
+        if (!File.Exists(sourcePath))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("File doesn't exist!");
+            Console.ResetColor();
+            continue;
+        }
+
+        break;
+    }
+
+    while (true)
+    {
+        Console.Write("Enter source file path: ");
+        destinationPath = Console.ReadLine()!;
+        if (!File.Exists(destinationPath))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("File doesn't exist!");
+            Console.ResetColor();
+            continue;
+        }
+
+        break;
+    }
+
+    if (sourcePath == destinationPath)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Your paths are the same!\n");
+        Console.ResetColor();
+        continue;
+    }
+    break;
+}
+
+CopyToFile(sourcePath, destinationPath);
+
+void CopyToFile(string sourceFile, string destinationFile)
+{
+    string text = File.ReadAllText(sourceFile);
+    File.WriteAllText(destinationFile, text);
+    
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"\nText {text} was successfully copied!");
+    Console.ResetColor();
+}
